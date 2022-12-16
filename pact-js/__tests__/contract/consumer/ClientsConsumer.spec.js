2a99a60c452964cf91ed2dfd0039147b1d8b87d5
Prarup Gurung <grgprarup@gmail.com>
2022-12-16 12:38:17 +0545

    Minor fixes

--- pact-js/__tests__/contract/consumer/ClientsConsumer.spec.js
+++ pact-js/__tests__/contract/consumer/ClientsConsumer.spec.js
@@ -3,6 +3,7 @@
 const { Matchers } = require("@pact-foundation/pact")
 const { getClients, postClient } = require("../../../src/consumer")
 
+
 describe("Clients Service", () => {
     const GET_EXPECTED_BODY = [{
         "firstName": "John",
@@ -29,28 +30,28 @@ describe("Clients Service", () => {
         beforeEach(() => {
             const interaction = {
                 state: "i have a list of clients",
-                uponReceiving: "a request for clients",
+                uponReceiving: "a request for all clients",
                 withRequest: {
                     method: "GET",
                     path: "/clients",
                     headers: {
-                        Accept: "application/json, text/plain, */*"
+                        Accept: "application/json, text/plain, */*",
                     },
                 },
                 willRespondWith: {
                     status: 200,
                     headers: {
-                        "Content-Type": "application/json; charset=utf-8"
+                        "Content-Type": "application/json; charset=utf-8",
                     },
-                    body: GET_EXPECTED_BODY
-                }
+                    body: GET_EXPECTED_BODY,
+                },
             }
             return provider.addInteraction(interaction)
         })
-        
-        test("returns correct body, header and statuscode", async() => {
+
+        test("returns correct body, header and statusCode", async() => {
             const response = await getClients()
-            expect(response.headers['content-type']).toBe('application/json; charset=utf-8')
+            expect(response.headers['content-type']).toBe("application/json; charset=utf-8")
             expect(response.data).toEqual(GET_EXPECTED_BODY)
             expect(response.status).toEqual(200)
         })
